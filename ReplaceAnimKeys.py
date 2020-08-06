0fd7b0ad20ad451517fa637c8c06545da8585ebc
isobko <33483867+isobko@users.noreply.github.com>
2020-08-06 15:01:29 +0300

    Update ReplaceAnimKeys.py

--- ReplaceAnimKeys.py
+++ ReplaceAnimKeys.py
@@ -1,7 +1,7 @@
 import maya.cmds as cmds
 
 REPLACE_NUMBER = 0 # The value to be replaced with
-COMPARE = 0.01 # Checking the value of the animation key to be replaced
+
 
 def replaceKey():
     objs = cmds.ls(selection=True)
@@ -9,9 +9,8 @@ def replaceKey():
         times = cmds.keyframe(item, query=True, timeChange=True)
         values = cmds.keyframe(item, query=True, valueChange=True)
         for i in values:
-            if i <= COMPARE:
-                cmds.setKeyframe(item, time=times[0], value=REPLACE_NUMBER, at='scaleX')
-                cmds.setKeyframe(item, time=times[0], value=REPLACE_NUMBER, at='scaleY')
-                cmds.setKeyframe(item, time=times[0], value=REPLACE_NUMBER, at='scaleZ')
+            cmds.setKeyframe(item, time=times[0], value=REPLACE_NUMBER, at='scaleX')
+            cmds.setKeyframe(item, time=times[0], value=REPLACE_NUMBER, at='scaleY')
+            cmds.setKeyframe(item, time=times[0], value=REPLACE_NUMBER, at='scaleZ')
 
 replaceKey()
